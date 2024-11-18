e1df17b113a2ccee38b3f922653179d6e33dfb61
Springer <zxllks@yahoo.com>
2024-11-18 16:27:49 +0800

    feat: external better than public, custom error better than require

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -10,11 +10,25 @@ pragma solidity ^0.8.19;
 contract Raffle {
     uint256 private immutable i_entranceFee;
 
+    error Raffle__SendMoreToEnterRaffle();
+
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    // more gas efficient: external is better than public
+    function enterRaffle() external payable {
+        // using string is not gas efficient!
+        // require(msg.value >= i_entranceFee, "Send More To Enter Raffle");
+
+        // refactor1: more gas efficient: custom error is better than require
+        // if (msg.value < i_entranceFee) {
+        //     revert Raffle__SendMoreToEnterRaffle();
+        // }
+
+        // refactor2: one line
+        if (msg.value < i_entranceFee) revert Raffle__SendMoreToEnterRaffle();
+    }
 
     function pickWinner() public {}
 
